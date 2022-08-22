bd62ce285cb09d1103ead433fbf0516ccbee6bcd
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-22 15:08:01 -0500

    Refactoring days of the week component to see if I can combine this component and dinnerCards

--- components/cards/daysOfTheWeek.js
+++ components/cards/daysOfTheWeek.js
@@ -3,17 +3,26 @@ import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function DaysOfTheWeek({ dayObj }) {
+function DaysOfTheWeek({ dayObj, dinnerObj }) {
   return (
     <div>
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{dayObj.day}</Card.Title>
+          {/* {dinnerObj.firebaseKey && } */}
         </Card.Body>
         <footer>
-          <Card.Link href="/dinners/new">
+          <Card.Link href="/dinners/new" passHref>
             <Button variant="success" className="add-btn">Add</Button>
           </Card.Link>
+          {dinnerObj.firebaseKey && (
+            <div className="edit-delete-footer">
+              <Card.Link href={`/dinners/edit/${dinnerObj.firebaseKey}`} passHref>
+                <Button variant="warning" className="edit-btn">Edit</Button>
+              </Card.Link>
+              <Button variant="danger" className="delete-btn">Delete</Button>
+            </div>
+          )}
         </footer>
       </Card>
     </div>
@@ -25,10 +34,16 @@ DaysOfTheWeek.propTypes = {
     day: PropTypes.string,
     firebaseKey: PropTypes.string,
   }),
+  dinnerObj: PropTypes.shape({
+    recipeId: PropTypes.string,
+    dayId: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
 };
 
 DaysOfTheWeek.defaultProps = {
   dayObj: {},
+  dinnerObj: {},
 };
 
 export default DaysOfTheWeek;
