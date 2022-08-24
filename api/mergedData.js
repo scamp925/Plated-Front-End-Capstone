df0e3118271f813442bcb3b52a12bc985aa83503
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-24 14:51:46 -0500

    Dinner cards renders differently depending if a recipe has been added to the card or not. If a recipe has been added, the dinner card now renders the recipe card, the edit button and the delete button. Used getDinnersByDay and set it to another State called dinnerObj. In my conditionals, I am looking to see if the dayObj's firebaseKey matches the dinnerObj's dayId. If it does, then it means there's a join table entity in firebase and it will pull the recipe card and render the two buttons. If not, then dinner card will only render the name of the day and the add button

--- api/mergedData.js
+++ api/mergedData.js
@@ -4,9 +4,11 @@ import { getSingleRecipe } from './recipesData';
 // // GET RECIPE ON DINNER CARD
 const getRecipeOnDinnerCard = (dayId) => new Promise((resolve, reject) => {
   getDinnersByDay(dayId).then((dinnersArray) => {
+    console.warn(dinnersArray, 'dinner array');
     const recipeIdPromises = dinnersArray.map((dinners) => getSingleRecipe(dinners.recipeId));
 
     Promise.all(recipeIdPromises).then((recipeArray) => {
+      console.warn(recipeArray);
       resolve(recipeArray[0]);
     });
   })
