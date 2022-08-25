e9fee38ab8ae2d627466b897de6684579aa22a0d
Sariah Campopiano <scampopiano925@gmail.com>
2022-08-24 22:43:57 -0500

    Took out unnecessary code on single view recipe card

--- components/cards/singleViewRecipeCard.js
+++ components/cards/singleViewRecipeCard.js
@@ -5,17 +5,6 @@ import Button from 'react-bootstrap/Button';
 
 function SingleViewRecipeCards({ recipeObj }) {
   return (
-  //     <Card>
-  //   <Card.Header as="h5">Featured</Card.Header>
-  //   <Card.Body>
-  //     <Card.Title>Special title treatment</Card.Title>
-  //     <Card.Text>
-  //       With supporting text below as a natural lead-in to additional content.
-  //     </Card.Text>
-  //     <Button variant="primary">Go somewhere</Button>
-  //   </Card.Body>
-  // </Card>
-
     <div>
       <Card>
         <Card.Body>
