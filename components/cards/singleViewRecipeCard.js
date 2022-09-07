84f644cec15fd94e6c3ee83bf93411f67e3b2c3a
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-06 19:01:43 -0500

    Added white space to styling to that the ingredients and directions are easy to read

--- components/cards/singleViewRecipeCard.js
+++ components/cards/singleViewRecipeCard.js
@@ -26,7 +26,7 @@ function SingleViewRecipeCard({ recipeObj, onUpdate }) {
           </header>
           <section className="recipe-details">
             <Card.Text>Total Time: {recipeObj.totalTime}</Card.Text>
-            <Card.Text>{recipeObj.leftovers === true ? 'Leftovers: Yes' : 'Leftovers: No'}</Card.Text>
+            <Card.Text>{recipeObj.leftovers === 'Yes' ? 'Leftovers: Yes' : 'Leftovers: No'}</Card.Text>
             <Card.Text>{recipeObj.preheat && 'Preheat Oven To:'} {recipeObj.preheat}</Card.Text>
             <Card.Text>{recipeObj.ingredients && 'Ingredients:'}</Card.Text>
             <Card.Text className="box-around-input">{recipeObj.ingredients}</Card.Text>
