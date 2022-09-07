202d7ac468498e07f86f23085e3c8e312b68857b
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-06 21:59:19 -0500

    Got plated image to be under the welcome text and sign in button. Still working on centering it

--- components/Signin.js
+++ components/Signin.js
@@ -5,31 +5,21 @@ import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div>
-      <h1
-        className="text-center d-flex flex-column justify-content-center align-content-center"
-        style={{
-          height: '90vh',
-          padding: '30px',
-          maxWidth: '500px',
-          margin: '20px 0px 0px 0px',
-          color: '#a44a3f',
-          position: 'fixed',
-        }}
-      >Welcome to Plated
-      </h1>
-      <Image src="/pngegg-mobile.png" alt="Plate with fork and knife" width="700" height="615" />
-      <button
-        type="button"
-        className="btn btn-outline-light btn-lg copy-btn"
-        onClick={signIn}
-        style={{
-          margin: '490px 0px 0px 160px',
-          position: 'fixed',
-        }}
-      >
-        Sign In
-      </button>
+    <div className="sign-in-container">
+      <div className="plated-img">
+        <Image src="/pngegg-mobile.png" alt="Plate with fork and knife" width="740" height="615" />
+      </div>
+      <div className="overlay-on-img">
+        <h1 className="welcome-header">Welcome to Plated
+        </h1>
+        <button
+          type="button"
+          className="btn btn-outline-light btn-lg copy-btn sign-in-btn"
+          onClick={signIn}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
