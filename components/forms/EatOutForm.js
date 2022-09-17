cee4998f03d0e1ddbf20ee2952751ce1dc753332
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-17 11:48:52 -0500

    Added files related to newest feature - eat out cards

---
+++ components/forms/EatOutForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function EatOutForm() {
+  return (
+    <div>
+      Eat Out
+    </div>
+  );
+}
+
+// EatOutForm.propTypes = {
+//   props: PropTypes.string.isRequired,
+// };
+
+export default EatOutForm;
