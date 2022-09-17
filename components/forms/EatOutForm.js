835b17e0f0ae9eadc92017a00f29d5bf70989082
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-17 14:19:01 -0500

    If restaurant is selected and form is submitted, then value on Firebase object for eatOut is created with the value of Restaurant. Same things goes for someone else's home, but it's value being different home

--- components/forms/EatOutForm.js
+++ components/forms/EatOutForm.js
@@ -1,16 +1,86 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createEatOutCard } from '../../api/eatOutData';
 
-function EatOutForm() {
+function EatOutForm({ eatOutObj }) {
+  const [formInput, setFormInput] = useState({});
+  const router = useRouter();
+  const { user } = useAuth();
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormInput((prevState) => ({
+  //     ...prevState,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = { ...formInput, uid: user.uid };
+    createEatOutCard(payload).then(() => {
+      router.push('/mealOptions/');
+    });
+  };
+
   return (
-    <div>
-      Eat Out
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <h2 className="title mt-3">{eatOutObj?.firebaseKey ? 'Update the' : 'Add a New'} Eat Out Option</h2>
+      <div className="margin-top" />
+      {['radio'].map((type) => (
+        <div key={`inline-${type}`} className="mb-3">
+          <Form.Label className="whereTo">Will you be going to a restaurant or someone's home?</Form.Label>
+          <Form.Check
+            className="yes-option"
+            inline
+            label="Restaurant"
+            name="whereTo"
+            type={type}
+            id={`inline-${type}-1`}
+            value="Restaurant"
+            checked={formInput.whereTo === 'Restaurant'}
+            onChange={(e) => setFormInput((prevState) => ({
+              ...prevState,
+              whereTo: e.target.value,
+            }))}
+          />
+          <Form.Check
+            inline
+            label="Someone's Home"
+            name="whereTo"
+            type={type}
+            id={`inline-${type}-2`}
+            value="Different Home"
+            checked={formInput.whereTo === 'Different Home'}
+            onChange={(e) => setFormInput((prevState) => ({
+              ...prevState,
+              whereTo: e.target.value,
+            }))}
+          />
+        </div>
+      ))}
+      <div className="form-btn">
+        <Button type="submit" variant="success">{eatOutObj?.firebaseKey ? 'Update' : 'Add'} Eat Out Option</Button>
+      </div>
+    </Form>
   );
 }
 
-// EatOutForm.propTypes = {
-//   props: PropTypes.string.isRequired,
-// };
+EatOutForm.propTypes = {
+  eatOutObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    placeName: PropTypes.string,
+    whereTo: PropTypes.string,
+  }),
+};
+
+EatOutForm.defaultProps = {
+  eatOutObj: {},
+};
 
 export default EatOutForm;
