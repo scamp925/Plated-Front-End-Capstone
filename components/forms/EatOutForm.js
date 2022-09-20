1487019b70126f00836f10ad9da4bb5219a7d0cc
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-19 19:53:22 -0500

    Able to type in the input and have the response go to firebase then render on the DOM on an eat out card

--- components/forms/EatOutForm.js
+++ components/forms/EatOutForm.js
@@ -64,6 +64,7 @@ function EatOutForm({ eatOutObj }) {
           />
           <section hidden={!formInput.whereTo}>
             <Form.Group className="mb-3" controlId="formBasicInput">
+              <Form.Label>Where are you going?</Form.Label>
               <Form.Control type="text" placeholder={formInput.whereTo === 'Different Home' ? "e.g. Lucy's house" : 'e.g. Olive Garden'} name="placeName" value={formInput.placeName} onChange={handleChange} />
             </Form.Group>
           </section>
