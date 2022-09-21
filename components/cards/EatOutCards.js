39df8c4c43d791c1362701aec4f282485e2312ed
Sariah Campopiano <scampopiano925@gmail.com>
2022-09-20 19:49:56 -0500

    Added new Promise to dinner data to get dinner by eatOutId which was then used in new merged data Promise to have a single eat out card deleted and if it was on the meal calendar, it would also be removed. All things work.

--- components/cards/EatOutCards.js
+++ components/cards/EatOutCards.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { deleteEatOutCardCompletely } from '../../api/mergedData';
 
-function EatOutCards({ dayObj, eatOutObj }) {
+function EatOutCards({ dayObj, eatOutObj, onUpdate }) {
+  const router = useRouter();
+
+  const deleteThisEatOutCard = () => {
+    if (window.confirm(`Warning! You are about to permanently delete ${eatOutObj.placeName}. Click "OK" if you wish to continue.`)) {
+      deleteEatOutCardCompletely(eatOutObj.firebaseKey).then(() => onUpdate()).then(() => {
+        router.push('/mealOptions/');
+      });
+    }
+  };
+
   return (
     <div>
       <Card style={{ width: '18rem' }} className="individual-cards">
@@ -17,7 +29,7 @@ function EatOutCards({ dayObj, eatOutObj }) {
           <Link href={`/eatOut/edit/${eatOutObj.firebaseKey}`} passHref>
             <Button variant="info" className="edit-btn">Edit</Button>
           </Link>
-          <Button variant="danger">Delete</Button>
+          <Button variant="danger" onClick={deleteThisEatOutCard}>Delete</Button>
         </footer>
         )}
       </Card>
@@ -35,6 +47,7 @@ EatOutCards.propTypes = {
     placeName: PropTypes.string,
     whereTo: PropTypes.string,
   }),
+  onUpdate: PropTypes.func.isRequired,
 };
 
 EatOutCards.defaultProps = {
